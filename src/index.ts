16561422d4cbc3fa2f59478f6b0b473c02f68892
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2024-04-09 18:02:20 +0200

    Add some more utils

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,27 @@
 export async function quit() {
-    fetch('https://godot-rpc/quit', {
+    return fetch('https://godot-rpc/quit', {
         method: 'POST',
     })
 }
 
 export async function openUrl(url: string) {
-    fetch('https://godot-rpc/open-url', {
+    return fetch('https://godot-rpc/open-url', {
         method: 'POST',
         body: JSON.stringify({ url }),
     })
+}
+
+export async function rpc(procedure: string, body: any) {
+    return fetch('https://godot-rpc/' + procedure, {
+        method: 'POST',
+        body: JSON.stringify(body),
+    })
+}
+
+export async function onEvent<T>(eventName: string, handler: (data: T) => void) {
+    if (window) {
+        window.addEventListener(eventName, (event: Event) => {
+            handler((event as CustomEvent<T>).detail)
+        })
+    }
 }
