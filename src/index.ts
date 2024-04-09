0ed8575381833093c59621d7b95413964a3e977b
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2024-04-09 18:08:40 +0200

    Add jsdoc

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
+/**
+ * Quits the client application.
+ * 
+ * @returns A promise that resolves into an empty response.
+ */
 export async function quit() {
     return fetch('https://godot-rpc/quit', {
         method: 'POST',
     })
 }
 
+/**
+ * Opens a URL in the system's default browser. 
+ * 
+ * By default, regular links opened within Selene will open within Selene, even if target="_blank" is specified. Use this function to open a URL in the system's default browser instead.
+ * 
+ * @example
+ * openUrl('https://google.com')
+ * 
+ * @param url The URL to open.
+ * @returns A promise that resolves into an empty response.
+ */
 export async function openUrl(url: string) {
     return fetch('https://godot-rpc/open-url', {
         method: 'POST',
@@ -11,6 +27,16 @@ export async function openUrl(url: string) {
     })
 }
 
+/**
+ * Calls a custom remote procedure you defined in a bundle.
+ * 
+ * @example
+ * rpc('my-procedure', { my: 'data' })
+ * 
+ * @param procedure The name of the procedure to call.
+ * @param body The body of the request.
+ * @returns A promise that resolves into the response.
+ */
 export async function rpc(procedure: string, body: any) {
     return fetch('https://godot-rpc/' + procedure, {
         method: 'POST',
@@ -18,6 +44,15 @@ export async function rpc(procedure: string, body: any) {
     })
 }
 
+/**
+ * Registers an event handler for a custom event you can fire from a bundle.
+ * 
+ * @example
+ * onEvent('my-event', (data) => console.log(data))
+ * 
+ * @param eventName The name of the event to listen for.
+ * @param handler The handler to call when the event is triggered.
+ */
 export async function onEvent<T>(eventName: string, handler: (data: T) => void) {
     if (window) {
         window.addEventListener(eventName, (event: Event) => {
